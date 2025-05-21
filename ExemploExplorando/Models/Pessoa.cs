5d4ddc182d444373dca6c17aabea37fe6ff11584
NatashaB-randao <natashabrandao57@hotmail.com>
2025-05-21 13:37:53 -0300

    Validações no GET e SET

--- ExemploExplorando/Models/Pessoa.cs
+++ ExemploExplorando/Models/Pessoa.cs
@@ -7,8 +7,44 @@ namespace ExemploExplorando.Models
 {
     public class Pessoa
     {
-        public string Nome { get; set; }
-        public int Idade { get; set; }
+        private string _nome;
+        private int _idade;
+        public string Nome
+        {
+            get
+            {
+                return _nome.ToUpper();
+            }
+
+            set
+            {
+                if (value == "")
+                {
+                    throw new Exception("O nome não pode ser vazio");
+                }
+
+                _nome = value;
+            }
+        }
+
+
+        public int Idade
+        {
+            get
+            {
+                return _idade;
+            }
+
+            set
+            {
+                if (value < 0)
+                {
+                    throw new Exception("A idade não pode ser negativa");
+                }
+
+                _idade = value;
+            }
+        }
 
         public void Apresentar()
         {
